4d6f806006d9bff60700ff865f81a07b6ae20c9e
Festus Phillip <st.festus4cruise@gmail.com>
2026-07-30 01:14:38 +0100

    testing config

--- src/config/emailClient.js
+++ src/config/emailClient.js
@@ -1,10 +1,6 @@
 import * as brevo from "@getbrevo/brevo";
 
-const apiInstance = new brevo.TransactionalEmailsApi();
+console.log("Brevo exports:");
+console.log(Object.keys(brevo));
 
-apiInstance.setApiKey(
-  brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
-);
-
-export default apiInstance;
+process.exit(0);
